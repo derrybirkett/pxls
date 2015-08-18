0165e8327b5d510ee7360ff76862827cee553128
Derry Birkett <derry@d2tstudio.com>
2015-08-19 00:00:20 +0200

    Add link

--- client/components/theme.jsx
+++ client/components/theme.jsx
@@ -15,6 +15,7 @@ Theme = React.createClass({
           primaryText={ this.props.theme.item }
           leftAvatar={ <Avatar src={ this.props.theme.thumbnail }/> }
           secondaryText={ this.props.theme.user }
+          href={this.props.theme.url + "?ref=d2tstudio"} 
           rightIcon={ <IconButton iconClassName="muidocs-icon-custom-github" tooltip="GitHub" /> }
         />
     )
